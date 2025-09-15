da4cebb5265e23f8ec4bc57e731e7dd9c4125453
Eduardo Salazar <esalazar@mellowoodmedical.com>
2025-09-15 15:04:42 -0500

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
